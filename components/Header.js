885021aecd77e194acdf129c2eb921cb0bade7a0
granitnebiu <granitnebiu@gmail.com>
2022-04-28 23:12:20 +0200

    added navbar component and the utils requests.js

--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,7 @@ export default function Header() {
         className="cursor-pointer active:brightness-110 "
         width={100}
         height={100}
-        alt=""
+        alt="IMBD movie db"
         src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
       />
     </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,19 @@
-import React from "react";
+import requests from "@/utils/requests";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const router = useRouter();
+  return (
+    <div className="flex select-none justify-center bg-gray-600 px-16 text-xl text-gray-200 lg:text-2xl">
+      {Object.entries(requests).map(([key, { title, url }]) => (
+        <h2
+          onClick={() => router.push(`?genre=${key}`)}
+          className="cursor-pointer p-6 hover:text-white active:text-red-400"
+          key={key}
+        >
+          {title}
+        </h2>
+      ))}
+    </div>
+  );
 }
