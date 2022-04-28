40170a95564fff14c3b8cf83dea9a58e56b17772
granitnebiu <granitnebiu@gmail.com>
2022-04-28 23:45:12 +0200

    adding serverSide function to show the results in client side

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Results() {
-  return <div>Results</div>;
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          <h1>{result.title}</h1>
+        </div>
+      ))}
+    </div>
+  );
 }
